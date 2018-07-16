cda3571378131056f41af5487037e3841ddf9ce8
Menlik Getachew <Menlik.Getachew@ibm.com>
2018-07-16 11:22:55 -0400

    added gitignore :wq

--- Assets/js/jquerytodos.js
+++ Assets/js/jquerytodos.js
@@ -1,1 +1,32 @@
-alert("halo")
+//check off specific todos by clicking
+$("li").click(function () {
+  // //check if it's checked off or not
+  //   //if gray and line through
+  //   if ($(this).css("color") === "rgb(128, 128, 128)") {
+  //     //turn black
+  //       $(this).css({
+  //         color: 'black',
+  //         textDecoration: 'none'});
+  //   }else {
+  //     //if black and line through
+  //       $(this).css({
+  //         //turn gray
+  //         color: 'gray',
+  //         textDecoration: 'line-through'
+  //       });
+  //   }
+    $(this).toggleClass("completed");
+});
+
+//click on X to delete todos
+$("span").click(function (event) {
+    $(this).parent().fadeOut(500, function () {
+        $(this).remove();
+    });
+    event.stopPropagation();
+})
+
+$("input[type = 'text']").keypress(function () {
+    console.log("enter text");
+});
+  
